ee53485ba1f2f225368e13052de9e3ad8369b3b1
Santosh Kalidindi <skalidindi8@gmail.com>
2023-04-26 14:16:33 -0700

    remove all semicolons

--- src/main/scala/Playground.scala
+++ src/main/scala/Playground.scala
@@ -4,12 +4,7 @@ import sorting.BubbleSort
 object Playground {
 
   def main(args: Array[String]): Unit = {
-//    val array         = createArray(10);
-//    val originalArray = array.clone();
-    val value = BinarySearch.search(Array(5, 9, 29, 90, 66), 90);
-    println(value);
-//    println(originalArray.mkString(","))
-//    println(sortedArray.mkString(","))
-//    println(originalArray.sorted.mkString(","))
+    val value = BinarySearch.search(Array(5, 9, 29, 90, 66), 90)
+    println(value)
   }
 }
