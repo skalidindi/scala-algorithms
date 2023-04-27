9225e996877ce9d110591c30ad652ab8b42f5384
Santosh Kalidindi <skalidindi8@gmail.com>
2023-04-26 22:54:00 -0700

    clean up merge sort

--- src/main/scala/sorting/MergeSort.scala
+++ src/main/scala/sorting/MergeSort.scala
@@ -9,32 +9,38 @@ object MergeSort {
     if (n <= 1) return array
 
     sort(array, 0, n - 1)
-
-    array
   }
 
-  private def sort(array: Array[Int], l: Int, r: Int): Unit = {
+  private def sort(array: Array[Int], l: Int, r: Int): Array[Int] = {
     if (l < r) {
       val m = (l + r) / 2
       sort(array, l, m)
       sort(array, m + 1, r)
       merge(array, l, m, r)
     }
+
+    array
   }
 
-  private def merge(array: Array[Int], l: Int, m: Int, r: Int): Unit = {
+  private def merge(array: Array[Int], l: Int, m: Int, r: Int): Array[Int] = {
     val n1 = m - l + 1
     val n2 = r - m
 
-    val left  = array.slice(l, n1 + l)
-    val right = array.slice(m + 1, n2 + m + 1)
+    val left  = array.slice(l, m + 1)
+    val right = array.slice(m + 1, r + 1)
 
     var i = 0
     var j = 0
     var k = l
 
-    while (i < n1 && j < n2) {
-      if (left(i) <= right(j)) {
+    while (k < r + 1) {
+      if (i > left.length - 1) {
+        array(k) = right(j)
+        j += 1
+      } else if (j > right.length - 1) {
+        array(k) = left(i)
+        i += 1
+      } else if (left(i) <= right(j)) {
         array(k) = left(i)
         i += 1
       } else {
@@ -44,16 +50,6 @@ object MergeSort {
       k += 1
     }
 
-    while (i < n1) {
-      array(k) = left(i)
-      i += 1
-      k += 1
-    }
-
-    while (j < n2) {
-      array(k) = right(j)
-      j += 1
-      k += 1
-    }
+    array
   }
 }
